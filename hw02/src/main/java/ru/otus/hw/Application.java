81a2b41a51bb6e4f70f099af1bfba67f491c87c8
ypivanov <ypivanov@mts.ru>
2024-10-21 14:09:16 +0300

    Исправления замечаний 1

--- hw02/src/main/java/ru/otus/hw/Application.java
+++ hw02/src/main/java/ru/otus/hw/Application.java
@@ -3,9 +3,11 @@ package ru.otus.hw;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.TestRunnerService;
 
 
+@PropertySource("classpath:application.properties")
 @ComponentScan
 public class Application {
     public static void main(String[] args) {
--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,12 +2,10 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@PropertySource("classpath:application.properties")
 @Data
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
