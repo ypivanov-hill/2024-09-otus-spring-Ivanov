f889c037a3b4dc5046bcb50b73d4c367d2522fd5
ypivanov <ypivanov@mts.ru>
2024-11-05 22:03:21 +0300

    Домашнее задание 4

--- hw04/src/main/java/ru/otus/hw/shell/ShellTestCommands.java
+++ hw04/src/main/java/ru/otus/hw/shell/ShellTestCommands.java
@@ -1,7 +1,6 @@
 package ru.otus.hw.shell;
 
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.util.Lists;
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -12,30 +11,24 @@ import ru.otus.hw.service.ResultService;
 import ru.otus.hw.service.StudentService;
 import ru.otus.hw.service.TestService;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
 @ShellComponent(value = "Testing Application Commands")
 @RequiredArgsConstructor
 public class ShellTestCommands {
 
     private final StudentService studentService;
+
     private final TestService testService;
+
     private final ResultService resultService;
+
     private final LocalizedIOService localizedMessagesService;
 
     private Student student;
 
-    private List<Object> availableCharsets = Lists.newArrayList((Charset.availableCharsets().keySet().stream().toArray()));
-
     @ShellMethod(value = "Login command", key = {"l", "login"})
-    public String login(){
+    public String login() {
         student = studentService.determineCurrentStudent();
-        if(student.firstName().isEmpty() || student.lastName().isEmpty()){
+        if (student.firstName().isEmpty() || student.lastName().isEmpty()) {
             student = null;
             return localizedMessagesService.getMessage("ShellCommand.not.logged.in");
         } else {
@@ -54,22 +47,8 @@ public class ShellTestCommands {
     }
 
     private Availability isStartTestCommandAvailable() {
-        System.out.println("Def " + Charset.defaultCharset().displayName());
-
-       // for (var s : Charset.availableCharsets().keySet()) {
-          //  System.out.println("Def " + s);
-        /*    for (var t : Charset.availableCharsets().keySet()) {
-                String result = new String(localizedMessagesService.getMessage("ShellCommand.logged.first").getBytes(StandardCharsets.UTF_8), Charset.forName(t));
-                System.out.println( "From " /*+ s + " to " + t + " result " + result);
-            }*/
-       // }
-
-
-       String availableCharset = availableCharsets.get(0).toString();
-        System.out.println("Def " +availableCharset);
-        availableCharsets.remove(0);
         return (student != null)
                 ? Availability.available()
-                : Availability.unavailable(new String (localizedMessagesService.getMessage("ShellCommand.logged.first").getBytes(Charset.defaultCharset()), Charset.forName(availableCharset)));
+                : Availability.unavailable(localizedMessagesService.getMessage("ShellCommand.logged.first"));
     }
 }
