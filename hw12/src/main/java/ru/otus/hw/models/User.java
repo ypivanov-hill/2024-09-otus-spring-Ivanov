04e017ce57f7ff9c16b50b047fa0cba301caea7c
ypivanov <ypivanov@mts.ru>
2025-01-03 14:18:22 +0300

    Домашнее задание 12

---
+++ hw12/src/main/java/ru/otus/hw/models/User.java
@@ -0,0 +1,25 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private List<String> roles;
+}
